29812709f35f611dbb88296ddd3986b56a1ca221
karan <pkaran4252@gmail.com>
2020-07-13 08:25:04 +0530

    Simple paino

---
+++ js/script.js
@@ -0,0 +1,161 @@
+var m1= document.getElementById("1");
+var m2= document.getElementById("2");
+var m3= document.getElementById("3");
+var m4= document.getElementById("4");
+var m5= document.getElementById("5");
+var m6= document.getElementById("6");
+var m7= document.getElementById("7");
+var m8= document.getElementById("8");
+var m9= document.getElementById("9");
+var m10= document.getElementById("10");
+var m11= document.getElementById("11");
+var m12= document.getElementById("12");
+var m13= document.getElementById("13");
+var m14= document.getElementById("14");
+var m15= document.getElementById("15");
+var m16= document.getElementById("16");
+var m17= document.getElementById("17");
+var m18= document.getElementById("18");
+var m19= document.getElementById("19");
+var m20= document.getElementById("20");
+var m21= document.getElementById("21");
+var m22= document.getElementById("22");
+var m23= document.getElementById("23");
+var m24= document.getElementById("24");
+var m25= document.getElementById("25");
+
+$(document).keypress(function(event){
+    
+    var key = event.which || event.keyCode;
+    console.log(key);
+    if (key === 65 || key === 97)
+    {
+        
+        $('#btn1').css("background-color", "yellow");
+        m1.currentTime=0;
+        m1.play();
+        $('#btn1').css("background-color", "red");
+
+        
+    }
+    else if(key === 66 || key === 98)
+    {
+        m2.currentTime=0;
+        m2.play();
+    }
+    else if (key === 67 || key === 99)
+    {
+        m3.currentTime=0;
+        m3.play();
+    }
+    else if(key === 68 || key === 100)
+    {
+        m4.currentTime=0;
+        m4.play();
+    }
+    else if (key === 69 || key === 101)
+    {
+        m5.currentTime=0;
+        m5.play();
+    }
+    else if(key === 70 || key === 102)
+    {
+        m6.currentTime=0;
+        m6.play();
+    }
+    else if (key === 71 || key === 103)
+    {
+        m7.currentTime=0;
+        m7.play();
+    }
+    else if(key === 72 || key === 104)
+    {
+        m8.currentTime=0;
+        m8.play();
+    }
+    else if (key === 73 || key === 105)
+    {
+        m9.currentTime=0;
+        m9.play();
+    }
+    else if(key === 74 || key === 106)
+    {
+        m10.currentTime=0;
+        m10.play();
+    }
+    else if (key === 75 || key === 107)
+    {
+        m11.currentTime=0;
+        m11.play();
+    }
+    else if(key === 76 || key === 108)
+    {
+        m12.currentTime=0;
+        m12.play();
+    }
+    else if (key === 77 || key === 109)
+    {
+        m13.currentTime=0;
+        m13.play();
+    }
+    else if(key === 78 || key === 110)
+    {
+        m14.currentTime=0;
+        m14.play();
+    }
+    else if (key === 79 || key === 111)
+    {
+        m15.currentTime=0;
+        m15.play();
+    }
+    else if(key === 80 || key === 112)
+    {
+        m16.currentTime=0;
+        m16.play();
+    }
+    else if(key === 81 || key === 113)
+    {
+        m17.currentTime=0;
+        m17.play();
+    }
+    else if (key === 82 || key === 114)
+    {
+        m18.currentTime=0;
+        m18.play();
+    }
+    else if(key === 83 || key === 115)
+    {
+        m19.currentTime=0;
+        m19.play();
+    }
+    else if (key === 84 || key === 116)
+    {
+        m20.currentTime=0;
+        m20.play();
+    }
+    else if(key === 85 || key === 117)
+    {
+        m21.currentTime=0;
+        m21.play();
+    }
+    else if (key === 86 || key === 118)
+    {
+        m22.currentTime=0;
+        m22.play();
+    }
+    else if(key === 87 || key === 119)
+    {
+        m23.currentTime=0;
+        m23.play();
+    }
+    else if (key === 88 || key === 120)
+    {
+        m24.currentTime=0;
+        m24.play();
+    }
+    else if(key === 89 || key === 121)
+    {
+        m25.currentTime=0;
+        m25.play();
+    }
+});
